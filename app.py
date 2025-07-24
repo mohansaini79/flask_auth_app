c1797d196a3140fb2ea27ca573fbb757a036e28a
mohansaini79 <mohansaini798330@gmail.com>
2025-07-24 17:36:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import re  # ✅ For regex validations
 
-app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # Replace this with an environment variable in production
+app = Flask(__name__
+app.secret_key = os.environ.get('SECRET_KEY', 'supersecretkey') # Replace this with an environment variable in production
 # MongoDB config
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
